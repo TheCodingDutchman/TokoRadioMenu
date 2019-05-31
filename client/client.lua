d5beb35c561c9096c11a0f16cbf5a32487e53790
GameMaster2030 <koekiemonster30@outlook.com>
2019-05-31 23:31:04 +0200

    Easier to edit now

--- client/client.lua
+++ client/client.lua
@@ -1,15 +1,15 @@
-local items = {"uit", "aan"}
-local portofoon = NativeUI.CreateSliderItem("Portoffon", items, 1, false)
-menu:AddItem(portofoon)
+local items = {"off", "on"}
+local radio1 = NativeUI.CreateSliderItem("Police radio", items, 1, false)
+menu:AddItem(radio1)
 menu.OnSliderChange = function(sender, item, index)
-    if item == portofoon then
+    if item == radio1 then
        	currentSelectedIndex = item:IndexToItem(index)
-       	if currentSelectedIndex == "uit" then
+       	if currentSelectedIndex == "off" then
        		if exports.tokovoip_script:isPlayerInChannel(1) == true then
                 exports.tokovoip_script:removePlayerFromRadio(1)
             end
        	end
-        if currentSelectedIndex == "uit" then
+        if currentSelectedIndex == "on" then
             if exports.tokovoip_script:isPlayerInChannel(1) == false then
                 exports.tokovoip_script:addPlayerFromRadio(1)
             end
