bcd6f3e8d06b86ce5188dc89be7d89c617ba533a
GameMaster2030 <koekiemonster30@outlook.com>
2019-05-31 23:46:59 +0200

    Create a chat command

--- client/client.lua
+++ client/client.lua
@@ -1,18 +1,24 @@
-local items = {"off", "on"}
-local radio1 = NativeUI.CreateSliderItem("Police radio", items, 1, false)
-menu:AddItem(radio1)
-menu.OnSliderChange = function(sender, item, index)
-    if item == radio1 then
-       	currentSelectedIndex = item:IndexToItem(index)
-       	if currentSelectedIndex == "off" then
-       		if exports.tokovoip_script:isPlayerInChannel(1) == true then
-                exports.tokovoip_script:removePlayerFromRadio(1)
+function createMenu()
+    local items = {"off", "on"}
+    local radio1 = NativeUI.CreateSliderItem("Police radio", items, 1, false)
+    menu:AddItem(radio1)
+    menu.OnSliderChange = function(sender, item, index)
+        if item == radio1 then
+            currentSelectedIndex = item:IndexToItem(index)
+            if currentSelectedIndex == "off" then
+                if exports.tokovoip_script:isPlayerInChannel(1) == true then
+                    exports.tokovoip_script:removePlayerFromRadio(1)
+                end
             end
-       	end
-        if currentSelectedIndex == "on" then
-            if exports.tokovoip_script:isPlayerInChannel(1) == false then
-                exports.tokovoip_script:addPlayerFromRadio(1)
+            if currentSelectedIndex == "on" then
+                if exports.tokovoip_script:isPlayerInChannel(1) == false then
+                    exports.tokovoip_script:addPlayerFromRadio(1)
+                end
             end
         end
     end
 end
+
+RegisterCommand("radio", function()
+    createMenu()
+end)
