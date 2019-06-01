d1b46505c94f9737faa29bfe5d5b0e68f28cd27d
GameMaster2030 <koekiemonster30@outlook.com>
2019-06-01 17:32:11 +0200

    Fixing some things

--- client/client.lua
+++ client/client.lua
@@ -1,51 +1,50 @@
+local _menuPool = NativeUI.CreatePool()
+
+-- local mainMenu = NativeUI.CreateMenu("Radio", "~b~Select TokoVoip radio", 0, 0) -- Description is not working? Getting a value is `nil` here.
+local mainMenu = NativeUI.CreateMenu("Radio", nil, 0, 0)
+_menuPool:Add(mainMenu)
+--[[ Error of that it says nil, this is the one of MouseControlsEnabled
+[   4252813] Error running call reference function for resource TokoRadioMenu: citizen:/scripting/lua/scheduler.lua:405: @TokoRadioMenu/NativeUILua-Reloaded/UIMenu/MenuPool.lua:168: attempt to call a nil value (global 'tobool')
+[   4252828] stack traceback:
+[   4252828] 	@TokoRadioMenu/NativeUILua-Reloaded/UIMenu/MenuPool.lua:168: in method 'MouseControlsEnabled'
+[   4252828] 	@TokoRadioMenu/client/client.lua:12: in upvalue 'ref'
+[   4252828] 	citizen:/scripting/lua/scheduler.lua:391: in function <citizen:/scripting/lua/scheduler.lua:390>
+[   4252844] 	[C]: in function 'xpcall'
+[   4252844] 	citizen:/scripting/lua/scheduler.lua:390: in function <citizen:/scripting/lua/scheduler.lua:389>
+[   4252844] stack traceback:
+[   4252844] 	[C]: in function 'error'
+[   4252859] 	citizen:/scripting/lua/scheduler.lua:405: in function <citizen:/scripting/lua/scheduler.lua:374>
+]]--
+
+-- _menuPool:MouseControlsEnabled(false) -- Same here again...
+
+local state = {"off", "on"}
+local radio1 = NativeUI.CreateSliderItem("Police radio", state, nil, "devider variable") -- Devider is hard coded to false(Line 43, in UIMenuSliderItem.lua). Same problem as above. Am I doing something wrong?
+mainMenu:AddItem(radio1)
+mainMenu.OnSliderChange = function(sender, item, index)
+if item == radio1 then
+currentSelectedIndex = item:IndexToItem(index)
+	if currentSelectedIndex == "off" then
+		if exports.tokovoip_script:isPlayerInChannel(1) == true then
+            exports.tokovoip_script:removePlayerFromRadio(1)
+        end
+    end
+    if currentSelectedIndex == "on" then
+		if exports.tokovoip_script:isPlayerInChannel(1) == false then
+			exports.tokovoip_script:addPlayerFromRadio(1)
+        end
+    end
+end
 RegisterCommand("radio", function()
     TriggerEvent('chat:addMessage', {
 	  color = { 255, 0, 0},
 	  multiline = true,
 	  args = {"TokoRadioMenu", "Command triggered."}
 	})
-    local _menuPool = NativeUI.CreatePool()
-    -- local _mainMenu = NativeUI.CreateMenu("Radio", "~b~Select TokoVoip radio", 0, 0) -- Description is not working? Getting a value is `nil` here.
-	local _mainMenu = NativeUI.CreateMenu("Radio", nil, 0, 0)
-    _menuPool:Add(_mainMenu)
-	--[[ Error of that it says nil, this is the one of MouseControlsEnabled
-	[   4252813] Error running call reference function for resource TokoRadioMenu: citizen:/scripting/lua/scheduler.lua:405: @TokoRadioMenu/NativeUILua-Reloaded/UIMenu/MenuPool.lua:168: attempt to call a nil value (global 'tobool')
-	[   4252828] stack traceback:
-	[   4252828] 	@TokoRadioMenu/NativeUILua-Reloaded/UIMenu/MenuPool.lua:168: in method 'MouseControlsEnabled'
-	[   4252828] 	@TokoRadioMenu/client/client.lua:12: in upvalue 'ref'
-	[   4252828] 	citizen:/scripting/lua/scheduler.lua:391: in function <citizen:/scripting/lua/scheduler.lua:390>
-	[   4252844] 	[C]: in function 'xpcall'
-	[   4252844] 	citizen:/scripting/lua/scheduler.lua:390: in function <citizen:/scripting/lua/scheduler.lua:389>
-	[   4252844] stack traceback:
-	[   4252844] 	[C]: in function 'error'
-	[   4252859] 	citizen:/scripting/lua/scheduler.lua:405: in function <citizen:/scripting/lua/scheduler.lua:374>
-	]]--
-
-    -- _menuPool:MouseControlsEnabled(false) -- Same here again...
-    local state = {"off", "on"}
-	TriggerEvent('chat:addMessage', {
-	  color = { 255, 0, 0},
-	  multiline = true,
-	  args = {"TokoRadioMenu", state}
-	})
-    local radio1 = NativeUI.CreateSliderItem("Police radio", state, nil, "devider variable") -- Devider is hard coded to false(Line 43, in UIMenuSliderItem.lua). Same problem as above. Am I doing something wrong?
-    _mainMenu:AddItem(radio1)
-    _mainMenu.OnSliderChange = function(sender, item, index)
-        if item == radio1 then
-            currentSelectedIndex = item:IndexToItem(index)
-            if currentSelectedIndex == "off" then
-                if exports.tokovoip_script:isPlayerInChannel(1) == true then
-                    exports.tokovoip_script:removePlayerFromRadio(1)
-                end
-            end
-            if currentSelectedIndex == "on" then
-                if exports.tokovoip_script:isPlayerInChannel(1) == false then
-                    exports.tokovoip_script:addPlayerFromRadio(1)
-                end
-            end
-        end
-    end
+	_menuPool:ProcessMenus()
+    mainMenu:Visible(not mainMenu:Visible())
 end)
+end
 
 --[[ Error when running command
 [   4496875] Error running call reference function for resource TokoRadioMenu: citizen:/scripting/lua/scheduler.lua:405: ...u/NativeUILua-Reloaded/UIMenu/items/UIMenuSliderItem.lua:92: attempt to call a nil value (global 'tobool')
